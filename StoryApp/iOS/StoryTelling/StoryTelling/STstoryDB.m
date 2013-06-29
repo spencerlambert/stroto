835df75e4e2566d3e13df68552a4f9f4983e6b0c
Spencer Lambert <spence@digis.net>
2013-06-29 15:49:18 -0600

    Adding story database class

---
+++ StoryApp/iOS/StoryTelling/StoryTelling/STstoryDB.m
@@ -0,0 +1,13 @@
+//
+//  STstoryDB.m
+//  StoryTelling
+//
+//  Created by Spencer Lambert on 6/29/13.
+//  Copyright (c) 2013 Aaswini. All rights reserved.
+//
+
+#import "STstoryDB.h"
+
+@implementation STstoryDB
+
+@end
