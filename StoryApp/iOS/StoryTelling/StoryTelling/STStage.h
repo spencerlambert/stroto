f4c66459c555410536253a3d5b6f9b29c5abc353
Spencer Lambert <spence@digis.net>
2013-06-30 13:23:49 -0600

    Changing STStage to type UIView

---
+++ StoryApp/iOS/StoryTelling/StoryTelling/STStage.h
@@ -0,0 +1,13 @@
+//
+//  STStage.h
+//  StoryTelling
+//
+//  Created by Spencer Lambert on 6/30/13.
+//  Copyright (c) 2013 Stroto, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STStage : UIView
+
+@end
---
+++ StoryApp/iOS/StoryTelling/StoryTelling/STStage.m
@@ -0,0 +1,13 @@
+//
+//  STStage.m
+//  StoryTelling
+//
+//  Created by Spencer Lambert on 6/30/13.
+//  Copyright (c) 2013 Stroto, LLC. All rights reserved.
+//
+
+#import "STStage.h"
+
+@implementation STStage
+
+@end
