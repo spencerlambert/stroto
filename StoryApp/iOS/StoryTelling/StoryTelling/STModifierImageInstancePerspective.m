cd8f825e970bb9513fa86db6c043ad9399b25d97
nstroto <vn_spencer_nandakumar@yahoo.com>
2013-08-19 11:48:31 +0530

    Comment correction

--- StoryApp/iOS/StoryTelling/StoryTelling/STModifierImageInstancePerspective.m
+++ StoryApp/iOS/StoryTelling/StoryTelling/STModifierImageInstancePerspective.m
@@ -14,25 +14,25 @@
     self = [super init];
     if(self)
     {
-        isGroundPerspective = -1;     //perspective : 0=ground, 1=sky, -1=No perspective.
+        isGroundPerspective = -1;     //perspective : 1=ground, 0=sky, -1=No perspective.
     }
     return self;
 }
 -(STImageInstancePosition*)getNewImageInstancePosition:(id)timeline
 {
-    //perspective : 0=ground, 1=sky, -1=No perspective.
+    //perspective : 1=ground, 0=sky, -1=No perspective.
     STImageInstancePosition *newImageInstancePosition = (STImageInstancePosition*)[(NSArray*)timeline lastObject];
     STImageInstancePosition *previousImageInstancePosition = (STImageInstancePosition*)[(NSArray*)timeline objectAtIndex:[(NSArray*)timeline count]-2];
     newImageInstancePosition->perspective = isGroundPerspective; //setting up perspective (1 or 0)
     int yChange =  newImageInstancePosition->y - previousImageInstancePosition->y; //gives delta y
-    if(isGroundPerspective == 1)
+    if(isGroundPerspective == 1) //ground
     {
         //moving image down along y axis(performing zoom in) , yChange = +ve
         //moving image up along y axis(zoom out) , yChange = -ve
         newImageInstancePosition->scale += (float)yChange * 0.001f;
     }
     
-    else if(isGroundPerspective == 0)
+    else if(isGroundPerspective == 0) //sky
     {
         //moving image down along y axis(performing zoom out) , yChange = +ve
         //moving image up along y axis(zoom in) , yChange = -ve
