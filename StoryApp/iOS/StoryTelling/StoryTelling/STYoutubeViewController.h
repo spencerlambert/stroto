a9c0b802b8ba38c1220c6fde1183bc0a893eef1b
vnspencer <spencerlambert@gmail.com>
2013-10-01 13:49:44 +0530

    Inserting Youtube files

---
+++ StoryApp/iOS/StoryTelling/StoryTelling/STYoutubeViewController.h
@@ -0,0 +1,16 @@
+//
+//  STYoutubeViewController.h
+//  StoryTelling
+//
+//  Created by Aaswini on 01/10/13.
+//  Copyright (c) 2013 Stroto, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GTLYouTube.h"
+
+@interface STYoutubeViewController : UIViewController
+
+@property (nonatomic, readonly) GTLServiceYouTube *youTubeService;
+
+@end
